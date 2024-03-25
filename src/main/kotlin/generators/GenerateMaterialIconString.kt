c702a575b29a23cd415fcf6585a7d60ba23dd42e
Sindre Moen <sindrenm@gmail.com>
2024-03-25 13:42:05 +0100

    Build entire file content in output

--- src/main/kotlin/generators/GenerateMaterialIconString.kt
+++ src/main/kotlin/generators/GenerateMaterialIconString.kt
@@ -3,9 +3,56 @@ package generators
 import models.VectorDrawable
 
 fun VectorDrawable.toImageVectorString(name: String): String {
-    return """
-        materialIcon(name = "$name") {
-            ${paths.joinToString("\n\n") { it.toMaterialIconPathString() }}
-        }
-    """.trimIndent()
+    val imports = """
+        |import androidx.compose.material.Icon
+        |import androidx.compose.material.icons.materialIcon
+        |import androidx.compose.runtime.Composable
+        |import androidx.compose.ui.graphics.Color
+        |import androidx.compose.ui.graphics.PathFillType
+        |import androidx.compose.ui.graphics.SolidColor
+        |import androidx.compose.ui.graphics.vector.ImageVector
+        |import androidx.compose.ui.graphics.vector.path
+        |import androidx.compose.ui.tooling.preview.Preview
+        |import com.sats.dna.icons.SatsIcons
+    """.trimMargin()
+
+    val paths = paths.joinToString("\n\n") {
+        it.toMaterialIconPathString()
+    }
+
+    val property = """
+        |@Suppress("UnusedReceiverParameter") // for convenient access
+        |val SatsIcons.$name: ImageVector
+        |    get() {
+        |        if (_$name != null) {
+        |            return _$name!!
+        |        }
+        |
+        |        _$name = materialIcon(name = "$name") {
+        |            $paths
+        |        }
+        |
+        |        return _$name!!
+        |    }
+    """.trimMargin()
+
+    val backingProperty = """
+        |@Suppress("ObjectPropertyName")
+        |private var _$name: ImageVector? = null
+    """.trimMargin()
+
+    val preview = """
+        |@Preview
+        |@Composable
+        |private fun SatsIcon${name}Preview() {
+        |    Icon(SatsIcons.$name, contentDescription = null)
+        |}
+    """.trimMargin()
+
+    return listOf(
+        imports,
+        property,
+        backingProperty,
+        preview,
+    ).joinToString("\n\n")
 }
